d6048c5d2eda2f7704efd8d1ce268c00efe7a5c6
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-11-29 19:20:08 +0100

    Update App.js with main application logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-// import Login from './pages/Login';
 import Register from './pages/Register';
+import Login from './pages/login';
+import Event from './pages/CreateEvent';
+import EditFormEvent from './components/EditFormEvent';
+import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InscriptionForm from './components/InscriptionForm';
 
 function App() {
+  const token = localStorage.getItem("token");
+console.log(token);
+
   return (
     <BrowserRouter>
     <Routes>
-      
-      {/* <Route path="/login" element={<Login />} /> */}
+    
+      <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register />} />
-     
+      <Route path="/dashboard"  element={token ? <Dashboard />:<Login/>} />
+      <Route path="/createEvent" element={<Event />} />
+      <Route path="/edit/:id" element={<EditFormEvent />} />
+      <Route path="/ins" element={<InscriptionForm />} />
     </Routes>
    
   </BrowserRouter>
