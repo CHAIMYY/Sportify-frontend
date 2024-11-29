c47590a508d34f79e04bd010f1de152ab30a181d
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-11-29 19:23:25 +0100

    Add or update CreateEvent page

---
+++ src/pages/CreateEvent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebare from "../components/Sidebar";
+import EventForm from "../components/EventForm";
+
+export default function createEvent() {
+    return (
+<>
+<Sidebare/>
+
+<EventForm/>
+
+
+
+
+</>
+
+
+
+    )};
