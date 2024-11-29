644ab5834b4b23b3c3dba88dd933264d499f1c4d
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-11-29 19:23:10 +0100

    Update Dashboard page with new features or layout

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebare from "../components/Sidebar";
+import EventCard from "../components/EventCard";
+
+export default function Dashboard() {
+    return (
+<>
+{/* <Sidebare/> */}
+
+<EventCard/>
+
+
+
+
+</>
+
+
+
+    )};
